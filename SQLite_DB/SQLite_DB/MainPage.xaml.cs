0b7fea2443e94899b043577a7c1939b9c9813aeb
jungmonster91 <jungmonster91@gmail.com>
2018-01-29 00:11:48 +0900

    add

--- SQLite_DB/SQLite_DB/MainPage.xaml.cs
+++ SQLite_DB/SQLite_DB/MainPage.xaml.cs
@@ -13,18 +13,39 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// 빈 페이지 항목 템플릿에 대한 설명은 https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x412에 나와 있습니다.
+using SQLite.Net.Platform.WinRT;
+using SQLite.Net.Interop;
+using SQLite.Net;
+using Windows.Storage;
 
 namespace SQLite_DB
 {
-    /// <summary>
-    /// 자체적으로 사용하거나 프레임 내에서 탐색할 수 있는 빈 페이지입니다.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+            testDB();
+        }
+
+        void testDB()
+        {
+            string pathLocal = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "database.sqlite");
+            ISQLitePlatform sqlitePlatform = new SQLitePlatformWinRT();
+
+            Person person = null;
+
+            var db = new SQLiteConnection(sqlitePlatform ,pathLocal);
+
+            db.CreateTable<Person>();
+            person = new Person { Name = "Minwoo" };
+            db.Insert(person);
+
+            var resulte = db.Query<Person>("SELECT * FROM PERSON WHERE (name = 'Minwoo');");
+            foreach(var tt in resulte)
+            {
+                TextTest.Text = string.Format("Person {0} {1}", tt.Id, tt.Name);
+            }
         }
     }
 }
